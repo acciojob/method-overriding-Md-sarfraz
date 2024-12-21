ae3a9196a285f373d514faac18eb96431608afc4
Md-sarfraz <mdsarfraz62070@gmail.com>
2024-12-22 01:09:07 +0530

    first commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,22 @@
 package com.driver;
 
 public class Main {
-  
+    public static class A{
+       public String meth(){
+            return "this is first method";
+        }
+    }
+    public static class B extends A{
+      public String meth(){
+            return "this is second method";
+        }
+    }
+
+    public static void main(String[] args) {
+        B obj =new B();
+        obj.meth();
+        B obj1=new B();
+        obj1.meth();
+
+    }
 }
